755b876bcba2cbdb1fe92cfb924d28ecd203911f
Elmer <elmermetrasbrum@gmail.com>
2023-01-16 13:23:19 -0300

    serve show PID

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: emetras- <emetras-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 18:48:22 by emetras-          #+#    #+#             */
-/*   Updated: 2023/01/16 10:38:48 by emetras-         ###   ########.fr       */
+/*   Updated: 2023/01/16 11:01:52 by emetras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 int main(void)
 {
-	signal(SIGUSR1, &sig_handler);
-	signal(SIGUSR2, &sig_handler);
+	//signal(SIGUSR1, &sig_handler);
+	//signal(SIGUSR2, &sig_handler);
 	ft_printf("Server PID: %d\n", getpid());
 	while (1)
 		pause();
 }
 
+// void	sig_handler()
+// {
+	
+// }
