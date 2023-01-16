fcc4df85ab9f0832da382a485b3e1aecd2bd4696
Elmer <elmermetrasbrum@gmail.com>
2023-01-16 10:43:59 -0300

    Added ft_printf & libft

--- Includes/header.h
+++ Includes/header.h
@@ -6,7 +6,7 @@
 /*   By: emetras- <emetras-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 16:44:02 by emetras-          #+#    #+#             */
-/*   Updated: 2023/01/10 16:45:47 by emetras-         ###   ########.fr       */
+/*   Updated: 2023/01/16 10:14:35 by emetras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define HEADER_H
 
 # include <signal.h>
+# include "../printf/header/ft_printf.h"
 
 #endif
