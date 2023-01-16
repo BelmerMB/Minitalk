46dd41b1b63713b57bbe59d017c5e9ec4a3f7d85
Elmer <elmermetrasbrum@gmail.com>
2023-01-16 10:44:56 -0300

    Added: send_message

--- src/client.c
+++ src/client.c
@@ -6,21 +6,24 @@
 /*   By: emetras- <emetras-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 16:51:58 by emetras-          #+#    #+#             */
-/*   Updated: 2023/01/13 23:56:12 by emetras-         ###   ########.fr       */
+/*   Updated: 2023/01/16 10:32:25 by emetras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/header.h"
 
+void	send_message(int pid, char *msg);
+void	send_char(int pid, char c);
+
 int	main (int argc, char **argv)
 {
 	unsigned int	pid;
 
-	pid = ft_atoi(argv[1]);
-	signal(SIGUSR1, sig_handler);
-	signal(SIGUSR2, sig_handler);
-	if ((argc == 3) && (pid > 0))
+	if ((argc == 3) && (ft_atoi(argv[1]) > 0))
+	{
+		pid = ft_atoi(argv[1]);
 		send_message(pid, argv[2]);
+	}
 	else
 	{
 		ft_printf("Error: Wrong number of arguments\n");
@@ -29,3 +32,31 @@ int	main (int argc, char **argv)
 	return (0);
 }
 
+void	send_message(int pid, char *msg)
+{
+	int index;
+
+	while (msg[index] != '\0')
+	{
+		send_char(pid, msg[index]);
+		//usleep(100);
+		index++;
+	}
+	send_char(pid, msg[index]);
+}
+
+void	send_char(int pid, char c)
+{
+	int index;
+
+	index = 0;
+	while (index < 8)
+	{
+		if ((c >> index) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		index++;
+		usleep(100);
+	}
+}
--- src/server.c
+++ src/server.c
@@ -6,11 +6,18 @@
 /*   By: emetras- <emetras-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 18:48:22 by emetras-          #+#    #+#             */
-/*   Updated: 2023/01/13 18:49:10 by emetras-         ###   ########.fr       */
+/*   Updated: 2023/01/16 10:38:48 by emetras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Includes/header.h"
+
 int main(void)
 {
-	
+	signal(SIGUSR1, &sig_handler);
+	signal(SIGUSR2, &sig_handler);
+	ft_printf("Server PID: %d\n", getpid());
+	while (1)
+		pause();
 }
+
